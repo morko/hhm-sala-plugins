9b941787045db6575bc135a012849aff1aae32ef
Oskari Pöntinen <mail.morko@gmail.com>
2019-05-22 17:13:08 +0300

    change the cron if interval get changed
    - added command to display motd

--- src/hr/motd.js
+++ src/hr/motd.js
@@ -27,11 +27,26 @@ room.onPlayerJoin = function(player) {
 
 if (room.pluginSpec.config.interval > 0) {
   displayMessageOnceIn(
-    room.pluginSpec.config.message,
-    room.pluginSpec.config.interval
+    room.pluginSpec.config.interval,
+    room.pluginSpec.config.message
   );
 }
 
 function displayMessageOnceIn(interval, message) {
-  room[`onCron${interval}Minutes`] = () => room.sendChat(message);
+  room[`onCron${interval}Minutes`] = () => room.sendChat(message + interval);
+}
+
+room.onCommand0_motd = (player) => {
+  const message = room.pluginSpec.config.message;
+  room.sendChat(message, player.id);
+}
+
+room.onConfigSet = ({paramName, newValue, oldValue}) => {
+  if (paramName === `interval`) {
+    // TODO: remove this when https://github.com/saviola777/haxball-headless-manager/issues/14 solved
+    if (!oldValue) oldValue = room.pluginSpec.config.interval;
+    const message = room.pluginSpec.config.message;
+    delete room[`onCron${oldValue}Minutes`];
+    displayMessageOnceIn(newValue, message);
+  }
 }
