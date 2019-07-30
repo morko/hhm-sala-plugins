10558e25f4b133ecaafb410db98fc799ed0bc658
Oskari Pöntinen <mail.morko@gmail.com>
2019-07-30 20:11:22 +0300

    added ability to enfore score and time limits

--- src/hr/game-mode.js
+++ src/hr/game-mode.js
@@ -25,10 +25,14 @@ room.pluginSpec = {
     ],
     // Locks the teams on room start if true. Does not enforce the setting.
     lockTeams: true,
-    // Sets the time limit on room start. Does not enforce the setting.
+    // Sets the time limit on room start. Does not enforce the setting by default.
     timeLimit: 3,
-    // Sets the score limit on room start. Does not enforce the setting.
-    scoreLimit: 3
+    // Enforce the time limit on game start.
+    enforceTimeLimit: false,
+    // Sets the score limit on room start. Does not enforce the setting by default.
+    scoreLimit: 3,
+    // Enforce the score limit on game start.
+    enforceScoreLimit: false,
   }
 };
 
@@ -46,10 +50,38 @@ function onStadiumChange(newMapName, byPlayer) {
   }
 }
 
+function onGameStart(byPlayer) {
+  if (!byPlayer || byPlayer.id === 0) return;
+
+  let scores = room.getScores();
+  if (!scores) return;
+
+  let timeLimit = room.getConfig('timeLimit') * 60;
+  let scoreLimit = room.getConfig('scoreLimit');
+  let hasTimeLimit = typeof timeLimit !== null || typeof timeLimit !== undefined;
+  let hasScoreLimit = typeof scoreLimit !== null || typeof scoreLimit !== undefined;
+
+  let enforceTimeLimit = room.getConfig('enforceTimeLimit');
+  let enforceScoreLimit = room.getConfig('enforceScoreLimit');
+
+  if (enforceTimeLimit && hasTimeLimit && timeLimit !== scores.timeLimit) {
+    room.sendAnnouncement(`This room allows only time limit of ${scoreLimit}.`, null, 0xFF0000);
+    room.stopGame();
+    room.setTimeLimit(timeLimit);
+    room.startGame();
+  } else if (enforceScoreLimit && hasScoreLimit && scoreLimit !== scores.scoreLimit) {
+    room.sendAnnouncement(`This room allows only score limit of ${scoreLimit}.`, null, 0xFF0000);
+    room.stopGame();
+    room.setScoreLimit(scoreLimit);
+    room.startGame();
+  }
+}
+
 room.onRoomLink = function onRoomLink() {
   room.setDefaultStadium(room.getConfig('defaultMap'));
   room.setTeamsLock(room.getConfig('lockTeams'));
   room.setTimeLimit(room.getConfig('timeLimit'));
   room.setScoreLimit(room.getConfig('scoreLimit'));
   room.onStadiumChange = onStadiumChange;
+  room.onGameStart = onGameStart;
 }
