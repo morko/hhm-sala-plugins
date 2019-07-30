f02df23840c1f5bae1e179e40e5eb65cc0d969ba
Oskari Pöntinen <mail.morko@gmail.com>
2019-07-30 19:24:38 +0300

    implemented missing function
    - added more messages
    - credited original author

--- src/hr/simple-commands.js
+++ src/hr/simple-commands.js
@@ -1,6 +1,9 @@
 /**
  * Haxball Headless Manager plugin for simple commands that are suitable using
  * with a public room.
+ * 
+ * Some code copied from XHerna's plugin.
+ * See original at https://github.com/XHerna/hhm-plugins/blob/master/src/tut/simple-commands.js.
  */
 
 const room = HBInit();
@@ -21,19 +24,39 @@ room.onCommand_bb = (player) => {
 
 room.onCommand_swap = (player) => {
   if (player.admin) {
-    swapTeams();
+    swapTeams(player);
+  } else {
+    room.sendAnnouncement('You need admin to use this command!', player.id, 0xFF0000);
   }
 };
 
 room.onCommand_rr = (player) => {
   if (player.admin) {
     restartGame();
+  } else {
+    room.sendAnnouncement('You need admin to use this command!', player.id, 0xFF0000);
   }
 };
 
 function restartGame() {
   room.stopGame();
   room.startGame();
+}
+
+function swapTeams(player) {
+  if (room.getScores() === null) {
+    players = room.getPlayerList();
+    for (i = 0; i < players.length; i++) {
+      if (players[i].team == 1) {
+        room.setPlayerTeam(players[i].id, 2);
+      } else if (players[i].team == 2) {
+        room.setPlayerTeam(players[i].id, 1);
+      }
+    }
+    room.sendAnnouncement('Teams swapped!', null, 0x00FF00);
+  } else {
+    room.sendAnnouncement('Game needs to be running to swap teams!', player.id, 0xFF0000);
+  }
 }
 
 let help = room.getPlugin(`sav/help`);
