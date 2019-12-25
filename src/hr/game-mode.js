cea62e3db7964ebc8df58deaf7cbbb017f99c773
Oskari Pöntinen <mail.morko@gmail.com>
2019-12-25 13:33:43 +0200

    fixes #8
    - implemented own maps plugin based on herna's plugin
    - game-mode allows the maps that are defined in the maps plugin

--- src/hr/game-mode.js
+++ src/hr/game-mode.js
@@ -1,12 +1,13 @@
 /**
  * This plugin for Headless Haxball Manager (HHM) restricts the rooms game mode
  * to given settings.
- * 
- * Works only with the default HaxBall maps!
- * 
+ *
  * By default only maps named `Big` and `Classic` are allowed.
- * 
- * The default map names in HaxBall are `Classic`, `Easy`, `Small`, `Big`, 
+ *
+ * Works with hr/maps plugin so that the maps that user has set there are
+ * automatically allowed.
+ *
+ * The default map names in HaxBall are `Classic`, `Easy`, `Small`, `Big`,
  * `Rounded`, `Hockey`, `Big Hockey`, `Big Easy`, `Big Rounded` and `Huge`.
  */
 
@@ -17,12 +18,9 @@ room.pluginSpec = {
   version: `1.0.0`,
   config: {
     // Map to load when starting the room.
-    defaultMap: 'Big',
-    // Array of allowed map names. 
-    allowedMaps: [
-      'Big',
-      'Classic'
-    ],
+    defaultMap: "Big",
+    // Array of allowed map names.
+    allowedMaps: ["Big", "Classic"],
     // Locks the teams on room start if true. Does not enforce the setting.
     lockTeams: true,
     // Sets the time limit on room start. Does not enforce the setting by default.
@@ -32,22 +30,66 @@ room.pluginSpec = {
     // Sets the score limit on room start. Does not enforce the setting by default.
     scoreLimit: 3,
     // Enforce the score limit on game start.
-    enforceScoreLimit: false,
+    enforceScoreLimit: false
   }
 };
 
-let currentMap = room.getConfig('defaultMap');
+let currentMap = room.getConfig("defaultMap");
 
 function onStadiumChange(newMapName, byPlayer) {
-  const allowedMaps = room.getConfig('allowedMaps');
+  const allowedMaps = room.getConfig("allowedMaps");
   if (!allowedMaps) return;
-  let allowedMap = allowedMaps.find((v) => v === newMapName);
+  let allowedMap = allowedMaps.find(v => v === newMapName);
+
   if (allowedMap) {
     currentMap = newMapName;
   } else {
-    room.setDefaultStadium(currentMap);
-    room.sendAnnouncement(`Allowed maps are: ${allowedMaps.join(', ')}.`, byPlayer.id, 0xFF0000);
+    if (isCustomMap(newMapName)) {
+      currentMap = newMapName;
+      return;
+    }
+    fallBackToPreviousMap(byPlayer);
   }
+}
+
+/**
+ * Checks if the given map was loaded with 'hr/maps' plugin.
+ * @param {string} mapName
+ */
+function isCustomMap(mapName) {
+  const mapsPlugin = room.getPlugin("hr/maps");
+  if (mapsPlugin) {
+    const maps = Object.keys(mapsPlugin.getConfig("maps"));
+    return maps.find(v => v === mapName);
+  }
+}
+
+/**
+ * Sets the map back to the currentMap.
+ * @param {object} [byPlayer] - Player that tried to change the map.
+ */
+function fallBackToPreviousMap(byPlayer) {
+  const mapsPlugin = room.getPlugin("hr/maps");
+
+  if (isCustomMap(currentMap)) {
+    mapsPlugin.setMap(currentMap);
+  } else {
+    room.setDefaultStadium(currentMap);
+  }
+
+  const allowedDefaultMaps = room.getConfig("allowedMaps")
+    ? room.getConfig("allowedMaps")
+    : [];
+  const allowedCustomMaps = Object.keys(mapsPlugin.getConfig("maps"));
+  const allowedMaps = allowedDefaultMaps.concat(allowedCustomMaps);
+
+  let playerId = byPlayer ? byPlayer.id : null;
+  if (playerId === 0) playerId = null;
+  room.sendAnnouncement(
+    `Allowed maps are: ${allowedMaps.join(", ")}.`,
+    playerId,
+    0xff0000
+  );
 }
 
 function onGameStart(byPlayer) {
@@ -56,21 +98,35 @@ function onGameStart(byPlayer) {
   let scores = room.getScores();
   if (!scores) return;
 
-  let timeLimit = room.getConfig('timeLimit') * 60;
-  let scoreLimit = room.getConfig('scoreLimit');
-  let hasTimeLimit = typeof timeLimit !== null || typeof timeLimit !== undefined;
-  let hasScoreLimit = typeof scoreLimit !== null || typeof scoreLimit !== undefined;
+  let timeLimit = room.getConfig("timeLimit") * 60;
+  let scoreLimit = room.getConfig("scoreLimit");
+  let hasTimeLimit =
+    typeof timeLimit !== null || typeof timeLimit !== undefined;
+  let hasScoreLimit =
+    typeof scoreLimit !== null || typeof scoreLimit !== undefined;
 
-  let enforceTimeLimit = room.getConfig('enforceTimeLimit');
-  let enforceScoreLimit = room.getConfig('enforceScoreLimit');
+  let enforceTimeLimit = room.getConfig("enforceTimeLimit");
+  let enforceScoreLimit = room.getConfig("enforceScoreLimit");
 
   if (enforceTimeLimit && hasTimeLimit && timeLimit !== scores.timeLimit) {
-    room.sendAnnouncement(`This room allows only time limit of ${scoreLimit}.`, null, 0xFF0000);
+    room.sendAnnouncement(
+      `This room allows only time limit of ${scoreLimit}.`,
+      null,
+      0xff0000
+    );
     room.stopGame();
     room.setTimeLimit(timeLimit);
     room.startGame();
-  } else if (enforceScoreLimit && hasScoreLimit && scoreLimit !== scores.scoreLimit) {
-    room.sendAnnouncement(`This room allows only score limit of ${scoreLimit}.`, null, 0xFF0000);
+  } else if (
+    enforceScoreLimit &&
+    hasScoreLimit &&
+    scoreLimit !== scores.scoreLimit
+  ) {
+    room.sendAnnouncement(
+      `This room allows only score limit of ${scoreLimit}.`,
+      null,
+      0xff0000
+    );
     room.stopGame();
     room.setScoreLimit(scoreLimit);
     room.startGame();
@@ -78,10 +134,10 @@ function onGameStart(byPlayer) {
 }
 
 room.onRoomLink = function onRoomLink() {
-  room.setDefaultStadium(room.getConfig('defaultMap'));
-  room.setTeamsLock(room.getConfig('lockTeams'));
-  room.setTimeLimit(room.getConfig('timeLimit'));
-  room.setScoreLimit(room.getConfig('scoreLimit'));
+  room.setDefaultStadium(room.getConfig("defaultMap"));
+  room.setTeamsLock(room.getConfig("lockTeams"));
+  room.setTimeLimit(room.getConfig("timeLimit"));
+  room.setScoreLimit(room.getConfig("scoreLimit"));
   room.onStadiumChange = onStadiumChange;
   room.onGameStart = onGameStart;
-}
+};
